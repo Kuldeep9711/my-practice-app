a5311ef5c2736896cded78486693af9bdd513636
Kuldeep Saini <k72125642@gmail.com>
2026-02-24 20:42:45 +0530

    add step and setStep count

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,56 +5,68 @@ import { useState } from "react";
 
 
 export default function Home() {
-// 1. State with Typescript type
+  // 1. State with Typescript type
   const [count, setCount] = useState<number>(0);
+  const [step, setStep] = useState<number>(1);
 
   // 2. Handler functions (also typed)
   const handleIncrement = () => {
-    setCount((prev) => prev + 1 );
+    setCount((prev) => prev + step);
   }
 
   const handleDecrement = () => {
-    setCount((prev) => prev - 1);
+    setCount((prev) => prev - step);
   }
 
   const handleReset = () => {
     setCount(0);
   }
 
+
+
   return (
     <main className="min-h-screen flex flex-col items-center justify-center bg-gray-50 p-4">
-       <h1 className="text-4xl font-bold mb-8 text-gray-800">
-           Simple Counter 
-       </h1>
+      <h1 className="text-4xl font-bold mb-8 text-gray-800">
+        Simple Counter
+      </h1>
 
-       <div className="text-6xl font-mono mb-10 text-blue-600">
-         {count}
-       </div>
+      <div className="text-6xl font-mono mb-10 text-blue-600">
+        {count}
+      </div>
 
-       <div className="flex gap-4 flex-wrap justify-center">
-        <Button 
-        label="Decrease"
-        onClick={handleDecrement}
-        variant="secondary"
+      <div className="flex gap-4 flex-wrap justify-center">
+        <Button
+          label="Decrease"
+          onClick={handleDecrement}
+          variant="secondary"
         />
 
-        <Button 
-        label="Increase"
-        onClick={handleIncrement}
-        variant="primary"
+        <Button
+          label="Increase"
+          onClick={handleIncrement}
+          variant="primary"
         />
 
-        <Button 
-        label="Reset"
-        onClick={handleReset}
-        disabled={count === 0}
-        variant="secondary"
+        <Button
+          label="Reset"
+          onClick={handleReset}
+          disabled={count === 0}
+          variant="secondary"
         />
-       </div>
+      </div>
 
-       <p className="mt-12 text-gray-500 text-sm">
-         You built this from memory (or with struggle - that's okay!)
-       </p>
+      <input
+
+        type="number"
+        value={step}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setStep(Number(e.target.value))}
+        className="w-20 px-2 py-1 border rounded text-center text-black mt-2"
+        min={1}
+      />
+
+      <p className="mt-5 text-gray-500 text-sm">
+        You built this from memory (or with struggle - that's okay!)
+      </p>
     </main>
   )
 }
